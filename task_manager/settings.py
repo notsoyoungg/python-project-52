0bac085e34b560d8039338dcc60f844643f1d7c9
notsoyoung2 <p.sergey1997@mail.ru>
2023-02-21 17:06:44 +0600

    linter fixes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -16,7 +16,6 @@ from dotenv import load_dotenv
 import dj_database_url
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, ".env"))
