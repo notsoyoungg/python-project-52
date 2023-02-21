464d71b3429a97b8e67b016016c9cab8d79579d8
notsoyoung2 <p.sergey1997@mail.ru>
2023-02-21 15:43:08 +0600

    prepare to deploy

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*y^*#%m!7o%searlf!xl#z_f-+s5hsgmhraloo8tf(7!krp4$'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -59,7 +64,7 @@ MIDDLEWARE = [
 ]
 
 ROLLBAR = {
-    'access_token': '0ed0d7fbecf44e5cac325ce654afc253',
+    'access_token': os.getenv("ROLLBAR_TOKEN"),
     'environment': 'development' if DEBUG else 'production',
     'code_version': '1.0',
     'root': BASE_DIR,
