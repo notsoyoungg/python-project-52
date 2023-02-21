7183befeaafc6e14e5db173c38746f3c60279b2b
notsoyoung2 <p.sergey1997@mail.ru>
2023-02-21 16:59:23 +0600

    fixes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG_OPTION", False))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'webserver', 'python-project-52-production-a798.up.railway.app']
 
@@ -97,12 +97,19 @@ AUTH_USER_MODEL = 'users.SiteUser'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+if DEBUG is True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-}
-
+    DATABASES = {
+        "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
